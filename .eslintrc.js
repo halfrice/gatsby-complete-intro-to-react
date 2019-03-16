003f129597d3e621545a0d5b1ce5f7891f0324b6
halfrice <halfrice@pm.me>
2019-03-15 22:30:52 -0700

    JSX

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,15 @@
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "prettier/react"],
-  plugins: [],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/errors",
+    "prettier",
+    "prettier/react",
+  ],
+  plugins: ["react", "import", "emotion"],
+  parser: "babel-eslint",
   parserOptions: {
-    ecmaVersion: 2016,
+    ecmaVersion: 2018,
     sourceType: "module",
     ecmaFeatures: {
       jsx: true,
@@ -12,5 +19,16 @@ module.exports = {
     es6: true,
     browser: true,
     node: true,
+  },
+  rules: {
+    "no-console": 1,
+    "no-unused-vars": ["warn", { args: "all" }],
+    "react/display-name": [0],
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react/prop-types": 0,
+    "emotion/no-vanilla": "error",
+    "emotion/import-from-emotion": "error",
+    "emotion/styled-import": "error",
   },
 }
---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-complete-intro-to-react`,
+  },
+  plugins: [`gatsby-plugin-eslint`, `gatsby-plugin-react-axe`],
+}
