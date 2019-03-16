ea51d601c5f3834120fd78892d8ba6101679ea4e
halfrice <halfrice@pm.me>
2019-03-15 21:38:57 -0700

    npm, ESLint & Prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from "react"
 
 const Pet = props => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+    React.createElement("h2", {}, props.breed),
+  ])
+}
 
 export default class Index extends React.Component {
   render() {
@@ -15,14 +15,14 @@ export default class Index extends React.Component {
       React.createElement(Pet, {
         name: "Luna",
         animal: "Dog",
-        breed: "Havanese"
+        breed: "Havanese",
       }),
       React.createElement(Pet, {
         name: "Pepper",
         animal: "Bird",
-        breed: "Cockatiel"
+        breed: "Cockatiel",
       }),
-      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
-    ]);
+      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+    ])
   }
 }
