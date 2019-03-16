c85898a31dfcdd1bb86fbd4831ecef9cfe44567a
halfrice <halfrice@pm.me>
2019-03-15 21:23:15 -0700

    Pure React

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,28 @@
-import React from "react"
+import React from "react";
 
-export default () => <div>Hello world!</div>
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
+
+export default class Index extends React.Component {
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
+    ]);
+  }
+}
